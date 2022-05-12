f3b383cab7c7c8f64490758b202130dfdb1a9b17
Paulo de Tarso <paulo.oinab@gmail.com>
2022-05-12 15:32:13 -0300

    2. Adicione as props necessárias ao componente de formulário

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 class Form extends React.Component {
   render() {
+    const {
+      cardName, //uma string;
+      cardDescription, //uma string;
+      cardAttr1, //uma string;
+      cardAttr2, //uma string;
+      cardAttr3, //uma string;
+      cardImage, //uma string;
+      cardRare, //uma string;
+      cardTrunfo, //um boolean;
+      hasTrunfo, //um boolean;
+      isSaveButtonDisabled, //um boolean;
+      onInputChange, //uma callback;
+      onSaveButtonClick, //uma callback;
+    } = this.props;
+
     return (
       <form>
         <fieldset>
@@ -9,14 +24,27 @@ class Form extends React.Component {
             <label htmlFor="nome">
               Nome:
               <br />
-              <input type="text" id="nome" name="nome" data-testid="name-input" />
+              <input
+                type="text"
+                id="nome"
+                name="nome"
+                data-testid="name-input"
+                value={cardName}
+                onChange={onInputChange}
+              />
             </label>
           </div>
           <div>
             <label htmlFor="descricao">
               Descrição:
               <br />
-              <textarea id="descricao" name="descricao" data-testid="description-input" />
+              <textarea
+                id="descricao"
+                name="descricao"
+                data-testid="description-input"
+                value={cardDescription}
+                onChange={onInputChange}
+              />
             </label>
           </div>
           <div>
@@ -28,6 +56,8 @@ class Form extends React.Component {
                 id="atributo1"
                 name="atributo1"
                 data-testid="attr1-input"
+                value={cardAttr1}
+                onChange={onInputChange}
               />
             </label>
           </div>
@@ -40,6 +70,8 @@ class Form extends React.Component {
                 id="atributo2"
                 name="atributo2"
                 data-testid="attr2-input"
+                value={cardAttr2}
+                onChange={onInputChange}
               />
             </label>
           </div>
@@ -52,6 +84,8 @@ class Form extends React.Component {
                 id="atributo3"
                 name="atributo3"
                 data-testid="attr3-input"
+                value={cardAttr3}
+                onChange={onInputChange}
               />
             </label>
           </div>
@@ -59,13 +93,26 @@ class Form extends React.Component {
             <label htmlFor="imagem">
               Imagem:
               <br />
-              <input type="text" id="imagem" name="imagem" data-testid="image-input" />
+              <input
+                type="text"
+                id="imagem"
+                name="imagem"
+                data-testid="image-input"
+                value={cardImage}
+                onChange={onInputChange}
+              />
             </label>
           </div>
           <div>
             <label htmlFor="raridade">
               Raridade:
-              <select id="raridade" name="raridade" data-testid="rare-input">
+              <select
+                id="raridade"
+                name="raridade"
+                data-testid="rare-input"
+                value={cardRare}
+                onChange={onInputChange}
+              >
                 <option value="normal">normal</option>
                 <option value="raro">raro</option>
                 <option value="muito raro">muito raro</option>
@@ -80,11 +127,20 @@ class Form extends React.Component {
                 id="strunfo"
                 name="strunfo"
                 data-testid="trunfo-input"
+                checked={cardTrunfo}
+                onChange={onInputChange}
               />
             </label>
           </div>
           <div>
-            <button type="button" data-testid="save-button">Salvar</button>
+            <button
+              type="button"
+              data-testid="save-button"
+              disabled={isSaveButtonDisabled}
+              onClick={onSaveButtonClick}
+            >
+              Salvar
+            </button>
           </div>
         </fieldset>
       </form>
