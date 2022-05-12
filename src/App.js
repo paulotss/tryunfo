671ed89d63eaafcda6008640473365852107e536
Paulo de Tarso <paulo.oinab@gmail.com>
2022-05-12 19:23:28 -0300

    8. Exiba a lista de cartas que estão salvas no estado

--- src/App.js
+++ src/App.js
@@ -100,6 +100,7 @@ class App extends React.Component {
       cardRare,
       cardTrunfo,
       hasTrunfo,
+      cards,
       isSaveButtonDisabled,
     } = this.state;
 
@@ -138,6 +139,21 @@ class App extends React.Component {
                 cardTrunfo={ cardTrunfo }
               />
             </aside>
+          </section>
+          <section className="show">
+            { cards.map((card, key) => (
+              <Card
+                key={ key }
+                cardName={ card.cardName }
+                cardDescription={ card.cardDescription }
+                cardAttr1={ card.cardAttr1 }
+                cardAttr2={ card.cardAttr2 }
+                cardAttr3={ card.cardAttr3 }
+                cardImage={ card.cardImage }
+                cardRare={ card.cardRare }
+                cardTrunfo={ card.cardTrunfo }
+              />
+            )) }
           </section>
         </main>
       </>
